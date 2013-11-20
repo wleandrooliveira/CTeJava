ab53e3613eb92f76ef9f8232ae12b7c4c1f0aa80
Henrique <henrique.lacerda@movetecnologia.com.br>
2013-11-20 19:53:57 -0200

    Adicionando modificacoes

--- src/br/com/moveti/erpmove/transporte/controller/FormatacaoDados.java
+++ src/br/com/moveti/erpmove/transporte/controller/FormatacaoDados.java
@@ -16,112 +16,81 @@ import java.util.zip.ZipOutputStream;
  *
  * @author Geraldo Henrique Lacerda Pinto
  */
-public class FormatacaoDados
-{
+public class FormatacaoDados {
+
     private static SimpleDateFormat formatIsoS = new SimpleDateFormat("yyyyMMdd");
     private static SimpleDateFormat formatIso = new SimpleDateFormat("yyyy-MM-dd");
     private static SimpleDateFormat formatBra = new SimpleDateFormat("dd/MM/yyyy");
     private static Date date;
 
-    public static String parseDataIsoSI(String dataIso)
-    {
-        try
-        {
-            if (!dataIso.isEmpty())
-            {
+    public static String parseDataIsoSI(String dataIso) {
+        try {
+            if (!dataIso.isEmpty()) {
                 date = formatIso.parse(dataIso);
                 return (formatIsoS.format(date));
-            }
-            else
-            {
+            } else {
                 return "";
             }
-        }
-        catch (ParseException e)
-        {
+        } catch (ParseException e) {
             e.printStackTrace();
             return ("Erro no parser da data");
         }
     }
 
-    public static String parseDataIsoS(String dataBra)
-    {
-        try
-        {
-            if (!dataBra.isEmpty())
-            {
+    public static String parseDataIsoS(String dataBra) {
+        try {
+            if (!dataBra.isEmpty()) {
                 date = formatBra.parse(dataBra);
                 return (formatIsoS.format(date));
-            }
-            else
-            {
+            } else {
                 return "";
             }
-        }
-        catch (ParseException e)
-        {
+        } catch (ParseException e) {
             e.printStackTrace();
             return ("Parse Date Error");
         }
     }
 
-    public static String parseDataIso(String dataBra)
-    {
-        try
-        {
-            if (!dataBra.isEmpty())
-            {
+    public static String parseDataIso(String dataBra) {
+        try {
+            if (!dataBra.isEmpty()) {
                 date = formatBra.parse(dataBra);
                 return (formatIso.format(date));
-            }
-            else
-            {
+            } else {
                 return "";
             }
-        }
-        catch (ParseException e)
-        {
+        } catch (ParseException e) {
             e.printStackTrace();
             return ("Parse Date Error");
         }
     }
 
-    public static String parseDataBra(String dataIso)
-    {
-        try
-        {
-            if (!dataIso.isEmpty())
-            {
+    public static String parseDataBra(String dataIso) {
+        try {
+            if (!dataIso.isEmpty()) {
                 date = formatIso.parse(dataIso);
                 return (formatBra.format(date));
-            }
-            else
-            {
+            } else {
                 return "";
             }
-        }
-        catch (ParseException e)
-        {
+        } catch (ParseException e) {
             e.printStackTrace();
             return ("Erro no parser da data");
         }
     }
 
-    public static boolean ValidaCpfCnpj(String s_aux)
-    {
+    public static boolean ValidaCpfCnpj(String s_aux) {
         System.out.println(s_aux);
         s_aux = s_aux.replace(".", "").replace("-", "").replace("/", "");
         //------- Rotina para CPF
-        if (s_aux.length() == 11)
-        {
+        if (s_aux.length() == 11) {
             int d1, d2;
             int digito1, digito2, resto;
             int digitoCPF;
             String nDigResult;
             d1 = d2 = 0;
             digito1 = digito2 = resto = 0;
-            for (int n_Count = 1; n_Count < s_aux.length() - 1; n_Count++)
-            {
+            for (int n_Count = 1; n_Count < s_aux.length() - 1; n_Count++) {
                 digitoCPF = Integer.valueOf(s_aux.substring(n_Count - 1, n_Count)).intValue();
                 //--------- Multiplique a ultima  casa por 2 a seguinte por 3 a seguinte por 4 e assim por diante.
                 d1 = d1 + (11 - n_Count) * digitoCPF;
@@ -132,24 +101,18 @@ public class FormatacaoDados
             //--------- Primeiro resto da divisão por 11.
             resto = (d1 % 11);
             //--------- Se o resultado for 0 ou 1 o digito é 0 caso contrário o digito é 11 menos o resultado anterior.
-            if (resto < 2)
-            {
+            if (resto < 2) {
                 digito1 = 0;
-            }
-            else
-            {
+            } else {
                 digito1 = 11 - resto;
             }
             d2 += 2 * digito1;
             //--------- Segundo resto da divisão por 11.
             resto = (d2 % 11);
             //--------- Se o resultado for 0 ou 1 o digito é 0 caso contrário o digito é 11 menos o resultado anterior.
-            if (resto < 2)
-            {
+            if (resto < 2) {
                 digito2 = 0;
-            }
-            else
-            {
+            } else {
                 digito2 = 11 - resto;
             }
             //--------- Digito verificador do CPF que está sendo validado.
@@ -158,99 +121,75 @@ public class FormatacaoDados
             nDigResult = String.valueOf(digito1) + String.valueOf(digito2);
             //--------- Comparar o digito verificador do cpf com o primeiro resto + o segundo resto.
             return nDigVerific.equals(nDigResult);
-        }
-        //-------- Rotina para CNPJ
-        else if (s_aux.length() == 14)
-        {
+        } //-------- Rotina para CNPJ
+        else if (s_aux.length() == 14) {
             int soma = 0, dig;
             String cnpj_calc = s_aux.substring(0, 12);
             char[] chr_cnpj = s_aux.toCharArray();
             //--------- Primeira parte
-            for (int i = 0; i < 4; i++)
-            {
-                if (chr_cnpj[i] - 48 >= 0 && chr_cnpj[i] - 48 <= 9)
-                {
+            for (int i = 0; i < 4; i++) {
+                if (chr_cnpj[i] - 48 >= 0 && chr_cnpj[i] - 48 <= 9) {
                     soma += (chr_cnpj[i] - 48) * (6 - (i + 1));
                 }
             }
-            for (int i = 0; i < 8; i++)
-            {
-                if (chr_cnpj[i + 4] - 48 >= 0 && chr_cnpj[i + 4] - 48 <= 9)
-                {
+            for (int i = 0; i < 8; i++) {
+                if (chr_cnpj[i + 4] - 48 >= 0 && chr_cnpj[i + 4] - 48 <= 9) {
                     soma += (chr_cnpj[i + 4] - 48) * (10 - (i + 1));
                 }
             }
             dig = 11 - (soma % 11);
             cnpj_calc += (dig == 10 || dig == 11)
-                ? "0" : Integer.toString(dig);
+                    ? "0" : Integer.toString(dig);
             //--------- Segunda parte
             soma = 0;
-            for (int i = 0; i < 5; i++)
-            {
-                if (chr_cnpj[i] - 48 >= 0 && chr_cnpj[i] - 48 <= 9)
-                {
+            for (int i = 0; i < 5; i++) {
+                if (chr_cnpj[i] - 48 >= 0 && chr_cnpj[i] - 48 <= 9) {
                     soma += (chr_cnpj[i] - 48) * (7 - (i + 1));
                 }
             }
-            for (int i = 0; i < 8; i++)
-            {
-                if (chr_cnpj[i + 5] - 48 >= 0 && chr_cnpj[i + 5] - 48 <= 9)
-                {
+            for (int i = 0; i < 8; i++) {
+                if (chr_cnpj[i + 5] - 48 >= 0 && chr_cnpj[i + 5] - 48 <= 9) {
                     soma += (chr_cnpj[i + 5] - 48) * (10 - (i + 1));
                 }
             }
             dig = 11 - (soma % 11);
             cnpj_calc += (dig == 10 || dig == 11)
-                ? "0" : Integer.toString(dig);
+                    ? "0" : Integer.toString(dig);
             return s_aux.equals(cnpj_calc);
-        }
-        else
-        {
+        } else {
             return false;
         }
     }
 
-    public static String PreparaFloat(String Valor)
-    {
+    public static String PreparaFloat(String Valor) {
         return Valor.replace(".", "").replace(",", ".");
     }
 
-    public static boolean VerificaConexaoInternet()
-    {
-        try
-        {
+    public static boolean VerificaConexaoInternet() {
+        try {
             java.net.URL mandarMail = new java.net.URL("http://www.google.com");
             java.net.URLConnection conn = mandarMail.openConnection();
 
             java.net.HttpURLConnection httpConn = (java.net.HttpURLConnection) conn;
             httpConn.connect();
             int x = httpConn.getResponseCode();
-            if (x == 200)
-            {
+            if (x == 200) {
                 return true;
-            }
-            else
-            {
+            } else {
                 return false;
             }
-        }
-        catch (java.net.MalformedURLException urlmal)
-        {
+        } catch (java.net.MalformedURLException urlmal) {
             //_ExibicaoErro.ShowError(urlmal);
             return false;
-        }
-        catch (java.io.IOException ioexcp)
-        {
+        } catch (java.io.IOException ioexcp) {
             //_ExibicaoErro.ShowError(ioexcp);
             return false;
         }
     }
 
-    public static String FormataNumerico(String Conteudo, int Tamanho, int TamanhoDecimal)
-    {
+    public static String FormataNumerico(String Conteudo, int Tamanho, int TamanhoDecimal) {
         String novoConteudo;
-        if (Conteudo.contains("."))
-        {
+        if (Conteudo.contains(".")) {
             System.out.println(Conteudo);
             String Buffer[] = Conteudo.split("\\.");
             System.out.println(Buffer[0]);
@@ -260,34 +199,25 @@ public class FormatacaoDados
             novoConteudo += FormatacaoDados.CortaString(Buffer[1], 2);
             TamanhoDecimal = TamanhoDecimal - Buffer[1].length();
             //System.out.println("TAMANHO DECIMAL "+TamanhoDecimal);
-            for (int i = 0; i < TamanhoDecimal; i++)
-            {
+            for (int i = 0; i < TamanhoDecimal; i++) {
                 novoConteudo += "0";
             }
-        }
-        else
-        {
+        } else {
             novoConteudo = Conteudo;
             novoConteudo += ".";
-            for (int i = 0; i < TamanhoDecimal; i++)
-            {
+            for (int i = 0; i < TamanhoDecimal; i++) {
                 novoConteudo += "0";
             }
         }
         return novoConteudo;
     }
 
-    public static String FormataString(String Conteudo, int Tamanho)
-    {
-        if (Conteudo.length() > Tamanho)
-        {
+    public static String FormataString(String Conteudo, int Tamanho) {
+        if (Conteudo.length() > Tamanho) {
             Conteudo = Conteudo.substring(0, Tamanho);
-        }
-        else
-        {
+        } else {
             String Buffer = "";
-            for (int i = 1; i <= (Tamanho - Conteudo.length()); i++)
-            {
+            for (int i = 1; i <= (Tamanho - Conteudo.length()); i++) {
                 Buffer += " ";
             }
             Conteudo = Conteudo + Buffer;
@@ -295,42 +225,31 @@ public class FormatacaoDados
         return Conteudo;
     }
 
-    public static String CortaString(String Conteudo, int Tamanho)
-    {
-        if (Conteudo.length() > Tamanho)
-        {
+    public static String CortaString(String Conteudo, int Tamanho) {
+        if (Conteudo.length() > Tamanho) {
             Conteudo = Conteudo.substring(0, Tamanho);
         }
         return Conteudo;
     }
 
-    public static String FormataString(String Conteudo, int Tamanho, String Preencher, String Lado)
-    {
-        if (Conteudo.length() > Tamanho)
-        {
+    public static String FormataString(String Conteudo, int Tamanho, String Preencher, String Lado) {
+        if (Conteudo.length() > Tamanho) {
             Conteudo = Conteudo.substring(0, Tamanho);
-        }
-        else
-        {
+        } else {
             String Buffer = "";
-            for (int i = 1; i <= (Tamanho - Conteudo.length()); i++)
-            {
+            for (int i = 1; i <= (Tamanho - Conteudo.length()); i++) {
                 Buffer = Preencher + Buffer;
             }
-            if (Lado.equals("E"))
-            {
+            if (Lado.equals("E")) {
                 Conteudo = Buffer + Conteudo;
-            }
-            else
-            {
+            } else {
                 Conteudo = Conteudo + Buffer;
             }
         }
         return Conteudo;
     }
 
-    public static String RemoverAcentuacao(String Conteudo)
-    {
+    public static String RemoverAcentuacao(String Conteudo) {
         Conteudo = Conteudo.replace("ç", "c");
         Conteudo = Conteudo.replace("Ç", "C");
         Conteudo = Conteudo.replace("á", "a");
